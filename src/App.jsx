b7c22bf498459d364b1ef88db70fc6e9e6b02577
Johnson <Ashane199080706@hotmail.com.tw>
2023-12-27 17:31:15 -0800

    init App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Hero, Demo } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 };
 
 export default App;
